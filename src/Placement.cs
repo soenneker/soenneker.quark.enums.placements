8aff3258a0baf4027f6549138affbdb433eef016
Jake Soenneker <jake@soenneker.com>
2025-09-18 08:23:58 -0500

    Initial commit

---
+++ src/Placement.cs
@@ -0,0 +1,18 @@
+﻿using Intellenum;
+
+namespace Soenneker.Quark.Enums.Placements;
+
+/// <summary>
+/// An enumeration for Quark, representing placement.
+/// </summary>
+[Intellenum<string>]
+public sealed partial class Placement
+{
+    public static readonly Placement Start = new("start");
+
+    public static readonly Placement End = new("end");
+
+    public static readonly Placement Top = new("top");
+
+    public static readonly Placement Bottom = new("bottom");
+}
---
+++ test/Soenneker.Quark.Enums.Placements.Tests/PlacementTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.Placements.Tests;
+
+public sealed class PlacementTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
