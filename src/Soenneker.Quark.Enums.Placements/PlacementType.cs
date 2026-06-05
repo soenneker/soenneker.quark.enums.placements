c92a01eeac98c39f574319b2100d984ce1975e6f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:16 -0500

    xml commenting

--- src/Soenneker.Quark.Enums.Placements/PlacementType.cs
+++ src/Soenneker.Quark.Enums.Placements/PlacementType.cs
@@ -8,11 +8,23 @@ namespace Soenneker.Quark;
 [EnumValue<string>]
 public sealed partial class PlacementType
 {
+    /// <summary>
+    /// The start.
+    /// </summary>
     public static readonly PlacementType Start = new("start");
 
+    /// <summary>
+    /// The end.
+    /// </summary>
     public static readonly PlacementType End = new("end");
 
+    /// <summary>
+    /// The top.
+    /// </summary>
     public static readonly PlacementType Top = new("top");
 
+    /// <summary>
+    /// The bottom.
+    /// </summary>
     public static readonly PlacementType Bottom = new("bottom");
 }
